b073ff4d4f806078a4dc39ca39b6416f9885c3d2
ShinSeongCheol <cher3725@naver.com>
2021-05-15 20:34:08 +0900

    8.AdivB.java

---
+++ 입출력과사칙연산/5.AplusB(Scanner).java
@@ -0,0 +1,15 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class AplusB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int A,B;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		System.out.println(A+B);
+	}
+}
