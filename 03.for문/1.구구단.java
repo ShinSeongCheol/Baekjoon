7bf69935392dc4131a808be8d430ea1acbc5e3cc
ShinSeongCheol <cher3725@naver.com>
2021-05-28 09:49:41 +0900

    1.구구단.java

---
+++ 03.for문/1.구구단.java
@@ -0,0 +1,38 @@
+package for문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class 구구단 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N;
+		
+		try {
+			N = Integer.parseInt(br.readLine());
+			
+			for(int i=1;i<10;i++) {
+				bw.write(N + " * " + i + " = " + N*i + "\n");
+			}
+			bw.flush();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				bw.close();
+				br.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
