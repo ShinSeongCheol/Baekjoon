6c990597f8aab646f4cc4b1dbdc2a90041dc077b
ShinSeongCheol <cher3725@naver.com>
2021-06-02 15:37:28 +0900

    9.별찍기_1.java

---
+++ 03.for문/9.별찍기_1.java
@@ -0,0 +1,37 @@
+package for¹®;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class º°Âï±â_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N;
+		
+		try {
+			N = Integer.parseInt(br.readLine());
+			
+			for(int i=1; i<=N; i++) {
+				for(int j=0; j<i; j++)
+					bw.write("*");
+				bw.write("\n");
+			}
+			bw.flush();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}try {
+			bw.close();
+			br.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
