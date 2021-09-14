d74ad779fb8144fe9c0c85fb80238448b6f67ea6
ShinSeongCheol <cher3725@naver.com>
2021-09-14 23:35:18 +0900

    01.피보나치_함수.java

--- 14.동적 계획법 1/01.피보나치_함수.java
+++ 14.동적 계획법 1/01.피보나치_함수.java
@@ -1,46 +1,40 @@
 package 동적_계획법_1;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class 피보나치_함수 {
-	public static int zero;
-	public static int one;
-	
-	public static void main(String[] args) throws IOException{
+	public static int[] memo;
+
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
+
 		int T = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<T; i++) {
-			zero = 0;
-			one = 0;
-			
+
+		for (int i = 0; i < T; i++) {
+			memo = new int[1000];
+
 			fibonacci(Integer.parseInt(br.readLine()));
-			
-			sb.append(zero).append(" ").append(one).append("\n");
+
+		}
+
+	}
+
+	public static int fibonacci(int n) {
+		if (n == 0) {
+			memo[n] = 0;
 		}
 		
-		System.out.print(sb);
-		br.close();
-	}
-	
-	public static int fibonacci(int n) {
-		if(n==0) {
-			zero++;
-			return 0;
+		if (n == 1) {
+			memo[n] = 1;
 		}
-		else if(n==1) {
-			one++;
-			return 1;
+		
+		if (memo[n] != 0) {
+			
 		}
-		else {
-			return fibonacci(n-1) + fibonacci(n-2);
-		}
+		return 0;
 	}
 }
