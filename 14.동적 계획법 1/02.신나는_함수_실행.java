cd386f47783488e405f86c507e2b070a95d02f1d
ShinSeongCheol <cher3725@naver.com>
2021-09-17 04:21:17 +0900

    02.신나는_함수_실행.java

--- 14.동적 계획법 1/02.신나는_함수_실행.java
+++ 14.동적 계획법 1/02.신나는_함수_실행.java
@@ -6,29 +6,45 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class 신나는_함수_실행 {
+	public static int[][][] memo = new int[51][51][51];
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int a, b, c;
+
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			
+			if((a==-1)&&(b==-1)&&(c==-1)) {
+				break;
+			}
+			
+			sb.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ").append(w(a,b,c)).append("\n");
+		}
 		
-		int a,b,c;
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		a = Integer.parseInt(st.nextToken());
-		b = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		
-		w(a,b,c);
+		System.out.println(sb);
 	}
 
 	public static int w(int a, int b, int c) {
-		if(a <=0 && b <=0 && c <= 0) {
+		if (a <= 0 || b <= 0 || c <= 0) {
 			return 1;
-		}else if(a > 20 && b > 20 && c > 20) {
-			return w(20,20,20);
-		}else if(a < b && b < c) {
-			return w(a,b,c-1) + w(a,b-1,c-1) - w(a,b-1,c);
-		}else {
-			return w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+		} else {
+			if (memo[a][b][c] != 0) {
+				return memo[a][b][c];
+			} else if (a > 20 || b > 20 || c > 20) {
+				return memo[a][b][c] = w(20, 20, 20);
+			} else if (a < b && b < c) {
+				return memo[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+			} else {
+				return memo[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+			}
 		}
+
 	}
 }
