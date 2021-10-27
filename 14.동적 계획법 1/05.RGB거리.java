feef44ad39982ea2503fe05ad3c9efe8bdecc0c4
ShinSeongCheol <cher3725@naver.com>
2021-10-27 11:30:48 +0900

    05.RGB거리.java

--- 14.동적 계획법 1/05.RGB거리.java
+++ 14.동적 계획법 1/05.RGB거리.java
@@ -1,4 +1,4 @@
-package algorythm;
+package 동적_계획법_1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,20 +21,21 @@ import java.util.StringTokenizer;
  * 집을 칠하는 비용은 1,000보다 작거나 같은 자연수이다.
 */
 
-public class test {
+public class RGB거리 {
 	
 	static int[][] rgb;
 	static int[][] dp;
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
 		rgb = new int[N][3];
 		dp = new int[N][3];
 		
 		for(int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			st = new StringTokenizer(br.readLine(), " ");
 			
 			for(int j=0; j<3; j++)
 				rgb[i][j] = Integer.parseInt(st.nextToken());
@@ -45,6 +46,23 @@ public class test {
 		dp[0][1] = rgb[0][1];
 		dp[0][2] = rgb[0][2];
 		
+		System.out.println(Math.min(Paint(N-1, 0), Math.min(Paint(N-1, 1), Paint(N-1, 2))));
+	}
+	
+	public static int Paint(int N, int color) {
+		if(dp[N][color] == 0) {
+			if(color == 0) {
+				dp[N][color] = Math.min(Paint(N-1, 1), Paint(N-1, 2)) + rgb[N][0];
+			}
+			else if(color == 1) {
+				dp[N][color] = Math.min(Paint(N-1, 0), Paint(N-1, 2)) + rgb[N][1];
+			}
+			else {
+				dp[N][color] = Math.min(Paint(N-1, 1), Paint(N-1, 0)) + rgb[N][2];
+			}
+		}
+			
+		return dp[N][color];	
 	}
 	
 }
