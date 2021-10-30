b694c04a8d2b1f71a5c1047ad31c87d2e39aff08
ShinSeongCheol <cher3725@naver.com>
2021-10-30 18:51:31 +0900

    06.정수_삼각형.java

--- 14.동적 계획법 1/06.정수_삼각형.java
+++ 14.동적 계획법 1/06.정수_삼각형.java
@@ -20,14 +20,17 @@ import java.util.StringTokenizer;
 
 public class Á¤¼ö_»ï°¢Çü {
 	
-	public static int[] num = new int[125250];
-	public static int[] dp;
+	public static int[][] arr;
+	public static int[][] dp;
 
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		dp = new int[n*n-1+1];
+		
+		arr = new int[n][n];
+		dp = new int[n][n];
+		
 		int j = 0;
 		
 		StringTokenizer st;
